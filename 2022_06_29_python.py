5f6428b4044e769f9b4608abdb7dc1cfd0e32c0c
suwhan2 <jerry5230@naver.com>
2022-06-29 09:46:33 +0900

    add new python file

--- 2022_06_29_python.py
+++ 2022_06_29_python.py
@@ -2,7 +2,7 @@ e_tot=0
 o_tot=0
 for x in range(1,101):
     if x%2 == 0: e_tot += x
-    else: o+toto += x
+    else: o_tot += x
 
 
-print("1-100까지 짝수 합:{0}, 홀수 합:{1}".fotmat(e_toto,o_tot))
+print("1-100까지 짝수 합:{0}, 홀수 합:{1}".format(e_tot,o_tot))
